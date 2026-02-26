ef6eae24bddab222aa063f99d73625743aaa0ed2
vmm73 <berat.karahan@uni-ulm.de>
2026-02-27 00:24:44 +0100

    Initial Commit

---
+++ DailyDee/DailyDeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyDeeApp.swift
+//  DailyDee
+//
+//  Created by Berat Karahan on 27.02.26.
+//
+
+import SwiftUI
+
+@main
+struct DailyDeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
